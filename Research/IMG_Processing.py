806b2fcf00e3d9f640494cfb967439d472875082
ic-me-helping <ilyas@ilyasclouse.me>
2026-02-02 17:25:10 -0500

    Add files via upload

---
+++ Research/IMG_Processing.py
@@ -0,0 +1,44 @@
+from PIL import ExifTags, Image
+
+def image_location(img_path): # Only functional for photos with GPS data and ExifTags
+    img = Image.open(img_path)
+    exif_reader = { ExifTags.TAGS[k]: v for k, v in img._getexif().items() if k in ExifTags.TAGS }
+
+    GPS_NS = exif_reader['GPSInfo'][1]
+    NS_list = exif_reader['GPSInfo'][2]
+    NS_DMS = f"{NS_list[0]} Degs, {NS_list[1]} Mins, {NS_list[2]} Secs"
+    NS_decimal = float(NS_list[0]) + float(NS_list[1])/60 + float(NS_list[2])/3600
+    
+    if GPS_NS == 'S':
+        NS_decimal = -NS_decimal
+    
+    print(GPS_NS)
+    print(NS_DMS)
+    print(round(NS_decimal, 5))
+
+    GPS_EW = exif_reader['GPSInfo'][3]
+    EW_list = exif_reader['GPSInfo'][4]
+    EW_DMS = f"{EW_list[0]} Degs, {EW_list[1]} Mins, {EW_list[2]} Secs"
+    EW_decimal = float(EW_list[0]) + float(EW_list[1])/60 + float(EW_list[2])/3600
+    
+    if GPS_EW == 'W':
+        EW_decimal = -EW_decimal
+    
+    print(GPS_EW)
+    print(EW_DMS)
+    print(round(EW_decimal, 5))
+
+    data = {
+        "Latitude": {
+            "Direction": GPS_NS,
+            "DMS": NS_DMS,
+            "Decimal": NS_decimal
+        },
+        "Longitude": {
+            "Direction": GPS_EW,
+            "DMS": EW_DMS,
+            "Decimal": EW_decimal
+        }
+    }
+    
+    return data
---
+++ pages/02_Image_Metadata.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import os
+import sys
+
+global BASE_DIR, RESEARCH_DIR
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+RESEARCH_DIR = os.path.join(BASE_DIR, "Research")
+sys.path.append(RESEARCH_DIR)
+from IMG_Processing import image_location
+
+# Upload image
+# Create a temporary directory if needed
+TEMP_DIR = "temp_uploads"
+if not os.path.exists(TEMP_DIR):
+    os.makedirs(TEMP_DIR)
+
+uploaded_images = st.file_uploader("Upload an image for OCR processing", type=["png", "jpg", "jpeg"], accept_multiple_files=False)
+
+
+# Display GPS Coordinates
+for uploaded_file in uploaded_images:
+
+    temp_file_path = os.path.join(TEMP_DIR, uploaded_file.name)
+    
+    with open(temp_file_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    
+    st.image(uploaded_file, caption="Uploaded Image", width="stretch")
+
+    # Extract GPS data
+    gps_data = image_location(temp_file_path)
+    st.subheader("Extracted GPS Data:")
+    st.json(gps_data)
+
+    # Delete temporary file
+    os.remove(temp_file_path)
