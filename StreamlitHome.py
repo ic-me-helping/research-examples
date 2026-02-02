0602fbdee532efeaa573e325c102b2e56602d77e
ic-me-helping <ilyas@ilyasclouse.me>
2026-02-02 17:58:49 -0500

    Update StreamlitHome.py

--- StreamlitHome.py
+++ StreamlitHome.py
@@ -5,4 +5,10 @@ import streamlit as st
 # Directory (Sidebar)
 ## NASA Power Data Query
 Download and view GeoJSON data from NASA Power API.
+
+## Image Processing
+Process images and gather information based off of available metadata and OCR.
+
+
 """
+
